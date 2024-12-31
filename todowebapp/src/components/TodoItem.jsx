c7ebfc1d90fbdc796361b9def9dceb915aa2b0ab
Arjun Nakkeeran <anakkeeran@ebsco.com>
2024-12-31 13:11:48 +0530

    Add subcomponent "Todo Item". Add function to delete, move up, move down the tasks.

---
+++ todowebapp/src/components/TodoItem.jsx
@@ -0,0 +1,21 @@
+﻿import PropTypes from 'prop-types';
+
+function TodoItem({ task, deleteTaskCallback, moveUpCallback, moveDownCallback }) {
+  return (
+      <li aria-label="task">
+          <span className="text">{task}</span>
+          <button className="delete-button" onClick={() => deleteTaskCallback()}> 🗑️</button>
+          <button className="up-button" onClick={() => moveUpCallback()}>⇧</button>
+          <button className="down-button" onClick={() => moveDownCallback()}>⇩</button>
+      </li>
+  );
+}
+
+TodoItem.propTypes = {
+    task: PropTypes.string.isRequired,
+    deleteTaskCallback: PropTypes.func.isRequired,
+    moveUpCallback: PropTypes.func.isRequired,
+    moveDownCallback: PropTypes.func.isRequired
+};
+
+export default TodoItem;
--- todowebapp/src/components/TodoList.jsx
+++ todowebapp/src/components/TodoList.jsx
@@ -1,6 +1,76 @@
+﻿import React, { useState } from 'react'
+import TodoItem from './Todoitem'
+
+const initialTasks = [
+    { id: self.crypto.randomUUID(), text: "Drink some coffee" },
+    { id: self.crypto.randomUUID(), text: "Create a todo app" },
+    { id: self.crypto.randomUUID(), text: "Drink some more coffee" }
+];
+
 function TodoList() {
-  return (
-    <h2>TODO App contents</h2>
+    const [tasks, setTasks] = useState(initialTasks);
+    const [newTaskText, setNewTaskText] = useState("");
+
+    function handleInputChange(event) {
+        setNewTaskText(event.target.value);
+    }
+
+    function addTask(event) {
+        if (newTaskText.trim() !== "") {
+            setTasks(t => [...t, { id: self.crypto.randomUUID(), text: newTaskText }]);
+            setNewTaskText("");
+        }
+        event.preventDefault();
+    }
+
+    function deleteTask(id) {
+        // from the event, find the record to be deleted.
+        const updatedTasks = tasks.filter(t => t.id != id);
+        setTasks(updatedTasks);
+    }
+
+    function moveTaskUp(index) {
+        if (index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+    function moveTaskDown(index) {
+        if (index >= 0 && index < tasks.length-1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+
+    return (
+      <article className="todo-list">
+        <h2>TODO</h2>
+        <form className="todo-input" aria-controls="todo-list" onSubmit={addTask}>
+            <input
+                    type="text"
+                    placeholder="Enter a task"
+                    required
+                    aria-label="Task Text"
+                    value={newTaskText}
+                    onChange={ handleInputChange} />
+            <button className="add-button" aria-label="Add task">Add</button>
+        </form>
+        <br/>
+        <h3>Task List</h3>
+        <ol id="todo-list" aria-live="polite" aria-label="task list">
+                {tasks.map((task, index) =>
+                    <TodoItem
+                        key={task.id}
+                        task={task.text}
+                        deleteTaskCallback={() => deleteTask(task.id)}
+                        moveUpCallback={() => moveTaskUp(index)}
+                        moveDownCallback={ () => moveTaskDown(index)}
+                    ></TodoItem>
+                )}
+        </ol>
+    </article>
   );
 }
 
